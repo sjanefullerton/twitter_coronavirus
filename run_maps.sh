bf4b9b5dd08c802a635a43f53c28f7dfd9ebe07a
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-08 16:20:04 -0800

    added run_maps.sh

--- run_maps.sh
+++ run_maps.sh
@@ -1,4 +1,6 @@
 # file will loop over each file in dataset and run the map.py command
-for file in '/data/Twitter dataset/'geoTwitter20*; do
+#!/bin/sh
+for file in /data/Twitter dataset/geoTwitter20-*.zip; do
+	echo "Running map.py on $file"
     ./src/map.py --input_path="$file" &
 done
