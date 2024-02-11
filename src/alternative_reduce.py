d1f66acd8be16f009130c4299b8c0b49fd23bf87
Sarah Fullerton <sjanesasha@gmail.com>
2024-02-11 13:10:15 -0800

    Updated README.md

---
+++ src/alternative_reduce.py
@@ -0,0 +1,10 @@
+# alternative reduce
+"""
+Input (on command line): a list of hashtags
+Output: a line plot where,
+1. There is one line per input hashtag
+2. The x-axis is the day of the year.
+3. The y-axis is the number of tweets that use that hashtag during the year.
+"""
+
+
